57564331aac9d4c7161ec6459ed2ded3264d2d07
tomassquintero <tomyq1405@gmail.com>
2026-07-09 21:56:40 -0300

    2026-07-09 21:56:40

--- arch/hypr/gamemode.sh
+++ arch/hypr/gamemode.sh
@@ -7,8 +7,9 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword animations:enabled 0;\
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
-        keyword decoration:rounding 0"
-    
+        keyword general:border_size 0;\
+        keyword decoration:rounding 0"    
+
     # Matar waybar
     killall waybar
     exit
